77a44cf36039e92fad2f9883c0212c9ce65c61a0
Александр Коробицын <iamkorobitsyn@icloud.com>
2024-06-06 13:44:14 +0300

    initial

---
+++ UIKitTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTestApp
+//
+//  Created by Александр Коробицын on 05.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
